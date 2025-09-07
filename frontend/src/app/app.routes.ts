3d9b3475bb868b903b2877c010a27dadeff89e98
RenatoXD4 <renatochicahual@gmail.com>
2025-09-06 21:07:37 -0400

    chore: Crear arquitectura de ejemplo para el proyecto en Angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'board',
+    loadChildren: () => import("./core/features/kanban/kanban.routes").then((m) => m.KANBAN_ROUTES),
+  },
+];

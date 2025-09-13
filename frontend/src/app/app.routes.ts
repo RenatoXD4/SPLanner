88ceaafce02246067be0cd572b62342ba0344045
AleGonzalez <Ale.gonzalezd@duocuc.cl>
2025-09-12 21:17:55 -0300

    Feat: Diseño de los formularios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: 'board',
     loadChildren: () => import("./core/features/kanban/kanban.routes").then((m) => m.KANBAN_ROUTES),
   },
+   {
+  path: 'login',
+     loadChildren: () => import('./core/features/auth/auth.routes').then( m => m.AUTH_ROUTES),
+  },
+ {
+  path: 'registro',
+     loadChildren: () => import('./core/features/auth/auth.routes').then( m => m.r),
+  },
+  {
+  path: 'recuperarContraseña',
+     loadChildren: () => import('./core/features/auth/auth.routes').then( m => m.recuperar),
+  },
 ];
--- frontend/src/app/core/features/auth/auth.routes.ts
+++ frontend/src/app/core/features/auth/auth.routes.ts
@@ -1,8 +1,22 @@
-import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { Routes} from '@angular/router';
 import { Login } from './login/login';
+import { Registro } from './registro/registro';
+import { Recuperar } from './recuperar/recuperar';
 
 // Este archivo define las rutas para /board/*
 export const AUTH_ROUTES: Routes = [
-  { path: '', component: Login },
-  { path: '', component: Login }, 
+  { path: '',  component: Login },
+
+
+
 ];
+
+export const r: Routes = [
+  { path: '',  component: Registro },
+
+];
+export const recuperar: Routes = [
+  { path : '', component: Recuperar}
+]
+

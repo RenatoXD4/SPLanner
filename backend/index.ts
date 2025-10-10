01e8361cace2724f18f07ededb6b777b0fd03131
RenatoXD4 <renatochicahual@gmail.com>
2025-10-10 20:16:53 -0300

    chore: Permitir localhost en el cors

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const api = "api-v1";
 
 // CORS manual simple
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   

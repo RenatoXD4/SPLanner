6a4dc7c127a535ea4ad6a1285cb86ca815f5c25d
RenatoXD4 <renatochicahual@gmail.com>
2025-10-14 22:08:07 -0300

    feat: Hacer editor de texto básico para la presentación y arreglar problema de que el botón editar y eliminar abrían el editor de texto.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';  // importar withFetch
+import { provideHttpClient, withFetch } from '@angular/common/http'; 
+import localeEsCl from '@angular/common/locales/es-CL';
 
+registerLocaleData(localeEsCl, 'es-CL');
 
 
 import { APP_ROUTES } from './app.routes'; // ✅ aquí cambias "routes" por "APP_ROUTES"
+import { registerLocaleData } from '@angular/common';
 
 
 export const appConfig: ApplicationConfig = {
@@ -16,6 +19,7 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(APP_ROUTES), // ✅ aquí también usas APP_ROUTES
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())  // activar fetch aquí
+    provideHttpClient(withFetch()),
+    { provide: LOCALE_ID, useValue: 'es-CL' }  // activar fetch aquí
   ]
 };

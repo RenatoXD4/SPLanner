3894994e5efe0e3104bd76043f7e977840d35bd3
RenatoXD4 <renatochicahual@gmail.com>
2025-11-12 13:16:58 -0300

    fix: Arreglo docker para que se pueda ejecutar

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -4,5 +4,10 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'dashboard/:id',
+    // Le dices a Angular que NO pre-renderice esta ruta
+    renderMode: RenderMode.Client 
   }
 ];
